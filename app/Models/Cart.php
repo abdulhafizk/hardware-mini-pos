99a08968f2b35c734e83f4a034e2edcb243f3678
abdulhafizk <132445084+abdulhafizk@users.noreply.github.com>
2024-07-15 19:32:49 +0700

    Cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     */
+    protected $fillable = ['user_id', 'product_id', 'quantity', 'price'];
 }
